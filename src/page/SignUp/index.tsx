d216c00c6b619bac6b77945efec093782522e1eb
wesley guerra <guerra_ssa@hotmail.com>
2020-08-27 12:34:34 -0300

    update

--- src/page/SignUp/index.tsx
+++ src/page/SignUp/index.tsx
@@ -1,102 +1,106 @@
-import React, {useCallback, useRef} from 'react';
-import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
-import { Form} from '@unform/web'; //
-import {FormHandles} from '@unform/core';
-import * as Yup from 'yup';
-import getValidationErrors from '../../utils/getValidationErros';
-import Button from '../../components/Button';  //SEPRANDO POR COMPONENTE
-import logoImg from '../../assets/logo.svg';
-import Input from '../../components/Input';    //SEPRANDO POR COMPENTENTE
-import { Container, Content, AimationContainer, Background } from './styles';
-import {Link, useHistory} from 'react-router-dom';
-import api from '../../services/api';
-import {useToast} from '../../hooks/Toast'
+import React, { useCallback, useRef } from "react";
+import { FiArrowLeft, FiMail, FiLock, FiUser } from "react-icons/fi";
+import { Form } from "@unform/web"; //
+import { FormHandles } from "@unform/core";
+import * as Yup from "yup";
+import getValidationErrors from "../../utils/getValidationErros";
+import Button from "../../components/Button"; //SEPRANDO POR COMPONENTE
+import logoImg from "../../assets/logo.svg";
+import Input from "../../components/Input"; //SEPRANDO POR COMPENTENTE
+import { Container, Content, AimationContainer, Background } from "./styles";
+import { Link, useHistory } from "react-router-dom";
+import api from "../../services/api";
+import { useToast } from "../../hooks/Toast";
 
-interface SignUpFormData{
-    name: string;
-    email: string;
-    password: string;
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
 }
 
+const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null); //PEGAND REFERENCIA DO FORMULARIO
+  const { addToast } = useToast();
+  const history = useHistory();
 
-const SignUp: React.FC = () => { 
+  const handleSubmit = useCallback(
+    async (data: SignUpFormData) => {
+      try {
+        formRef.current?.setErrors({});
 
-    const formRef = useRef<FormHandles>(null); //PEGAND REFERENCIA DO FORMULARIO
-    const {addToast} = useToast();
-    const  history = useHistory();
- 
- const handleSubmit = useCallback(async(data: SignUpFormData)=>{
-     try {
-        formRef.current?.setErrors({})
-       
-         const schema = Yup.object().shape({   //VALIDAÇÃO USANDO YUP
-             name: Yup.string().required('Nome obrigatótio'),
-             email:Yup.string()
-             .required('E-mail obrigatótio')
-             .email('Digite um e-mail válido'),
-             password:Yup.string().min(6, 'No mínimo 6 dígitos'),
-         });
+        const schema = Yup.object().shape({
+          //VALIDAÇÃO USANDO YUP
+          name: Yup.string().required("Nome obrigatótio"),
+          email: Yup.string()
+            .required("E-mail obrigatótio")
+            .email("Digite um e-mail válido"),
+          password: Yup.string().min(6, "No mínimo 6 dígitos"),
+        });
 
-         await schema.validate(data,{abortEarly:false});
+        await schema.validate(data, { abortEarly: false });
 
-         await api.post('/users', data);
+        await api.post("/users", data);
 
-         history.push('/')
+        history.push("/");
 
-         addToast({
-             type: 'success',
-             title: 'Cadastro realizado',
-             description: 'Você já pode fazer seu logon no GoBarber!',
-
-         })
-         
-     } catch (err) {
+        addToast({
+          type: "success",
+          title: "Cadastro realizado",
+          description: "Você já pode fazer seu logon no GoBarber!",
+        });
+      } catch (err) {
         if (err instanceof Yup.ValidationError) {
-            const errors = getValidationErrors(err);
-            formRef.current?.setErrors(errors);
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
 
-            return;
+          return;
         }
         addToast({
-            type: 'error',
-            title: 'error no cadastro',
-            description: 'Ocerreu um erro ao ao fazer cadastro, tente novamente',
+          type: "error",
+          title: "error no cadastro",
+          description: "Ocerreu um erro ao ao fazer cadastro, tente novamente",
         });
-        }  
- },[addToast, history])
- 
-  return  (
+      }
+    },
+    [addToast, history]
+  );
 
-        <Container>
-            <Background />
-            <Content>
-                <AimationContainer>
-                <img src={logoImg} alt="GoBarber" />
-                
-                {/* DADOS SÃO PASSADOS PELO (useField)  @unform/core*/}
-                <Form  ref={formRef}  onSubmit={handleSubmit}> 
-    
-                    <h1>Faça seu cadastro</h1>
-    
-                    <Input name="name" icon={FiUser} placeholder="Nome" />
-                    <Input name="email" icon={FiMail} type="email" placeholder="E-mail" />
-                    <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
-    
-                    <Button type="submit"> Cadastrar </Button>
-    
-                </Form>
-    
-                <Link to="/">
-                    <FiArrowLeft />
-                    Voltar para logon
-                </Link>
-                </AimationContainer>
-               
-            </Content>
-    
-        </Container>
-    );
-}
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <AimationContainer>
+          <img src={logoImg} alt="GoBarber" />
 
+          {/* DADOS SÃO PASSADOS PELO (useField)  @unform/core*/}
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Faça seu cadastro</h1>
 
-export default SignUp
+            <Input name="name" icon={FiUser} placeholder="Nome" />
+            <Input
+              name="email"
+              icon={FiMail}
+              type="email"
+              placeholder="E-mail"
+            />
+            <Input
+              name="password"
+              icon={FiLock}
+              type="password"
+              placeholder="Senha"
+            />
+
+            <Button type="submit"> Cadastrar </Button>
+          </Form>
+
+          <Link to="/">
+            <FiArrowLeft />
+            Voltar para logon
+          </Link>
+        </AimationContainer>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import {Switch} from 'react-router-dom';
-import Route from './Route'
+import React from "react";
+import { Switch } from "react-router-dom";
+import Route from "./Route";
 
-import SignIn from '../page/Signin';
-import SignUp from '../page/SignUp';
+import SignIn from "../page/Signin";
+import SignUp from "../page/SignUp";
 
-import Dashboard from '../page/Dashboard';
+import Dashboard from "../page/Dashboard";
 
-
-const Routes: React.FC = () =>{
-    return(
-        <Switch>
-            <Route path="/" exact component={SignIn}/>
-            <Route path="/signup" exact component={SignUp}/>
-
-            <Route path="/dashboard" exact component={Dashboard} isPrivate />
-        
-        </Switch>
-    )
-}
+const Routes: React.FC = () => {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/signup" exact component={SignUp} />
+      <Route path="/dashboard" exact component={Dashboard} isPrivate />
+    </Switch>
+  );
+};
 
 export default Routes;
