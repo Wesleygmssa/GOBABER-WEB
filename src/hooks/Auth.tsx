5698a049da7afef1502ef96b0e1b42ed0b622b47
wesley guerra <guerra_ssa@hotmail.com>
2020-09-04 13:17:27 -0300

    update

--- src/hooks/Auth.tsx
+++ src/hooks/Auth.tsx
@@ -1,74 +1,67 @@
-import React, { createContext, useCallback, useState, useContext, } from 'react';
-import api from '../services/api';
+import React, { createContext, useCallback, useState, useContext } from "react";
+import api from "../services/api";
 
 interface SingnInCredentials {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
 interface AuthContextData {
-    user: Object;
-    signIn(credentials: SingnInCredentials): Promise<void>;
-    signOut(): void;
+  user: Object;
+  signIn(credentials: SingnInCredentials): Promise<void>;
+  signOut(): void;
 }
 
 interface AuthState {
-    token: string;
-    user: Object;
-};
+  token: string;
+  user: Object;
+}
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider: React.FC = ({ children }) => {
-    const [data, setData] = useState<AuthState>(() => {
-        const token = localStorage.getItem('@GoBarber:token'); //PEGANDO NO NAVEGADOR
-        const user = localStorage.getItem('@GoBarber:user');
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem("@GoBarber:token"); //PEGANDO NO NAVEGADOR
+    const user = localStorage.getItem("@GoBarber:user");
 
-        if (token && user) {
-            return { token, user: JSON.parse(user) };
-        }
-
-        return {} as AuthState;
-    })
-
-    const signIn = useCallback(async ({ email, password }) => {
-
-        const response = await api.post('sessions', { email, password }); // RETORNO DA API DO BACK-END
-
-        const { token, user } = response.data;
-
-        localStorage.setItem('@GoBarber:token', token); //SALVANDO NO NAVEGADOR
-        localStorage.setItem('@GoBarber:user', JSON.stringify(token));
-
-        setData({ token, user });
-
-    }, []);
-
-    const signOut = useCallback(()  =>{
-    localStorage.removeItem('@GoBarber:token'); //PEGANDO NO NAVEGADOR
-    localStorage.removeItem('@GoBarber:user');
-
-    setData({} as AuthState);
-    }, [])
-
-    return (
-        <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
-
-export function useAuth(): AuthContextData {
-    const context = useContext(AuthContext);
-
-    if (!context) {
-        throw new Error('useAuth must be used within an AuthProvider')
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
     }
 
-    return context;
+    return {} as AuthState;
+  });
+
+  const signIn = useCallback(async ({ email, password }) => {
+    const response = await api.post("sessions", { email, password }); // RETORNO DA API DO BACK-END
+
+    const { token, user } = response.data;
+
+    localStorage.setItem("@GoBarber:token", token); //SALVANDO NO NAVEGADOR
+    localStorage.setItem("@GoBarber:user", JSON.stringify(token));
+
+    setData({ token, user });
+  }, []);
+
+  const signOut = useCallback(() => {
+    localStorage.removeItem("@GoBarber:token"); //PEGANDO NO NAVEGADOR
+    localStorage.removeItem("@GoBarber:user");
+
+    setData({} as AuthState);
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+
+  return context;
 }
-
-
-
-
-
